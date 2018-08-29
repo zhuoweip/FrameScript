bf31303f383015eff272053ebe4de58ca57b35d1
SKY-20160222BER\Administrator <2826593725@qq.com>
2018-08-29 22:18:32 +0800

    2018_08_29 1.增加fps显示

--- ScriptsFrame/Assets/Script/Script/Frame/UICore/BaseUI.cs
+++ ScriptsFrame/Assets/Script/Script/Frame/UICore/BaseUI.cs
@@ -58,12 +58,29 @@ namespace UICore
         protected override void OnGUI()
         {
             base.OnGUI();
+            if (GUI.skin.label.fontSize != 40)
+                GUI.skin.label.fontSize = 40;
+
             DebugOnGUI();
+            ShowFPS();
         }
 
         private Dictionary<GameDebugLog.LogLevel, GameDebugLog.LevelSetting> levelSettings;
         private int lastLogIndex;
         private int logIndex;
+        private int lastFpsIndex;
+        private int fpsIndex;
+
+        private void ShowFPS()
+        {
+            if (Input.GetKeyDown(KeyCode.F10) && fpsIndex == lastFpsIndex)
+                fpsIndex = 1 - lastFpsIndex;
+            else if (Input.GetKeyUp(KeyCode.F10) && fpsIndex != lastFpsIndex)
+                lastFpsIndex = fpsIndex;
+
+            if (fpsIndex == 0) return;
+            GUILayout.Label(string.Format("FPS:{0}", 1 / Time.smoothDeltaTime));
+        }
 
         #region Debug输出
         private void DebugOnGUI()
@@ -101,7 +118,6 @@ namespace UICore
                 GameDebugLog.logRect = new Rect(Screen.width - 402, Screen.height - 268, 400, 266);
             }
 
-            GUI.skin.label.fontSize = 40;
             Color backgroundColor = GUI.backgroundColor;
             Color oldContentColor = GUI.contentColor;
             GUI.backgroundColor = new Color(0.5f, 0.0f, 0.0f, 0.5f);
---
+++ ScriptsFrame/Assets/tt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UICore;
+
+public class tt : BaseUI {
+
+    protected override void OnGUI()
+    {
+        base.OnGUI();
+    }
+}
