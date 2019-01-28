f1b28148092914e73c00903abe89f0b9d47e26ba
HZY <2826593725@qq.com>
2019-01-28 16:20:16 +0800

    20190128 1.增加uniRx

--- ScriptsFrame/Assets/Other/test1.cs
+++ ScriptsFrame/Assets/Other/test1.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using System;
+using System.Threading;
 
 public class test1 : MonoBehaviour {
 
     public RawImage rimg;
 
-	// Use this for initialization
-	void Start () {
-        transform.ReverseChild();
+    void Start()
+    {
+        var stream1 = Observable.Start(() =>
+        {
+            Thread.Sleep(4000);//4000毫秒
+            print("等待了4秒");
+            return 1;
+        }); //线程1
+        var stream2 = Observable.Start(() =>
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            print("等待2秒");
+            return 2;
+        }); //线程2
+
+
+        Observable.WhenAll(stream1, stream2).ObserveOnMainThread().Subscribe(_ => //当线程执行完毕，将其他线程中的值，返回到主线程中
+        {
+            print(_[0]);
+            print(_[1]);
+        });
+    }
+
+
+    private IEnumerator Wait()
+    {
+        yield return Observable.Timer(TimeSpan.FromSeconds(1)).ToYieldInstruction();
     }
 	
 	// Update is called once per frame
--- ScriptsFrame/Assets/Script/Frame/UICore/DataManager.cs
+++ ScriptsFrame/Assets/Script/Frame/UICore/DataManager.cs
@@ -17,6 +17,7 @@ public class DataManager : UnitySingleton<DataManager> {
         style.normal.textColor = Color.red;
 
         Cursor.visible = bool.Parse(Configs.Instance.LoadText("开启鼠标", "false/true"));
-        Debug.logger.logEnabled = bool.Parse(Configs.Instance.LoadText("开启日志", "false/true"));
+        //Debug.logger.logEnabled = bool.Parse(Configs.Instance.LoadText("开启日志", "false/true"));
+        Debug.logger.filterLogType = LogType.Error;//仅保留错误日志
     }
 }
