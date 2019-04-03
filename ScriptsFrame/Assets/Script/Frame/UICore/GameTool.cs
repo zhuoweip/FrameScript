29808616a56c246f334b392059add6afa2318a73
HZY <2826593725@qq.com>
2019-04-03 11:11:20 +0800

    20190403 1.增加gametool

--- ScriptsFrame/Assets/Script/Frame/UICore/GameTool.cs
+++ ScriptsFrame/Assets/Script/Frame/UICore/GameTool.cs
@@ -750,6 +750,31 @@ public static class CoroutineUtil
             UnityEngine.Object.DontDestroyOnLoad(_surrogate);
         }
     }
+    
+    /// <summary>
+    /// 协程等待 Allow for a routine to be yielded within another Coroutine without calling StartCoroutine
+    /// e.g. yield return RoutineUtil.Sub(DoSomething());
+    /// </summary>
+    public static IEnumerator Sub(IEnumerator routine)
+    {
+        while (true)
+        {
+            if (!routine.MoveNext())
+                yield break;
+
+            yield return routine.Current;
+        }
+    }
+
+    /// <summary>
+    /// Allow for sequential routines in one fell swoop.
+    /// e.g. yield return RoutineUtil.Sequence(DoSomething(), DoAnotherThing());
+    /// </summary>
+    public static IEnumerator Sequence(params IEnumerator[] routines)
+    {
+        for (int i = 0; i < routines.Length; i++)
+            yield return Sub(routines[i]);
+    }
     #endregion
 }
 #endregion
@@ -2742,6 +2767,154 @@ public static class LinqUtil
         return clone;
     }
 
+    public static void Fill<T>(T[] src, T data)
+    {
+        for (int i = 0, max = src.Length; i < max; i++)
+        {
+            src[i] = data;
+        }
+    }
+
+    public static T[][] NewDoubleArray<T>(int numRow, int numCol)
+    {
+        T[][] ret = new T[numRow][];
+        for (int i = 0; i < numRow; i++)
+        {
+            ret[i] = new T[numCol];
+        }
+
+        return ret;
+    }
+
+    public static void Shuffle(Array array)
+    {
+        for (int i = 0, max = array.Length; i < max; i++)
+        {
+            int r = UnityEngine.Random.Range(i, max);
+            object obj = array.GetValue(i);
+            object robj = array.GetValue(r);
+            array.SetValue(robj, i);
+            array.SetValue(obj, r);
+        }
+    }
+
+    public static void Shuffle(Array array, int start, int length)
+    {
+        for (int i = start; i < length; i++)
+        {
+            int r = UnityEngine.Random.Range(i, length);
+            object obj = array.GetValue(i);
+            object robj = array.GetValue(r);
+            array.SetValue(robj, i);
+            array.SetValue(obj, r);
+        }
+    }
+
+    public static T[] RemoveAt<T>(T[] src, int index)
+    {
+        T[] dest = new T[src.Length - 1];
+        if (index > 0)
+        {
+            Array.Copy(src, 0, dest, 0, index);
+        }
+
+        if (index < src.Length - 1)
+        {
+            Array.Copy(src, index + 1, dest, index, src.Length - index - 1);
+        }
+
+        return dest;
+    }
+
+    public static void RemoveAtIndex<T>(ref T[] src, int index)
+    {
+        T[] dest = new T[src.Length - 1];
+        if (index > 0)
+        {
+            Array.Copy(src, 0, dest, 0, index);
+        }
+
+        if (index < src.Length - 1)
+        {
+            Array.Copy(src, index + 1, dest, index, src.Length - index - 1);
+        }
+
+        src = dest;
+    }
+
+    public static void InsertAfter<T>(ref T[] src, int index, T val)
+    {
+        if (index == src.Length - 1)
+        {
+            System.Array.Resize(ref src, src.Length + 1);
+            src[src.Length - 1] = val;
+        }
+        else
+        {
+            T[] dest = new T[src.Length + 1];
+
+            Array.Copy(src, dest, index + 1);
+
+            dest[index + 1] = val;
+
+            Array.Copy(src, index + 1, dest, index + 2, src.Length - (index + 1));
+
+            src = dest;
+        }
+    }
+
+    public static bool Contains(string[] src, string itm)
+    {
+        if (src != null)
+        {
+            for (int i = 0; i < src.Length; i++)
+                if (src[i] == itm)
+                    return true;
+        }
+        return false;
+    }
+
+    public static bool Contains<T>(T[] src, T itm) where T : UnityEngine.Object
+    {
+        if (src != null)
+        {
+            for (int i = 0; i < src.Length; i++)
+                if (src[i] == itm)
+                    return true;
+        }
+        return false;
+    }
+
+    public static bool Contains(string[] src, int startIndex, int count, string itm)
+    {
+        if (src != null)
+        {
+            for (int i = 0; i < count; i++)
+                if (src[i + startIndex] == itm)
+                    return true;
+        }
+        return false;
+    }
+
+    public static bool Contains<T>(T[] src, int startIndex, int count, T itm) where T : UnityEngine.Object
+    {
+        if (src != null)
+        {
+            for (int i = 0; i < count; i++)
+                if (src[i + startIndex] == itm)
+                    return true;
+        }
+        return false;
+    }
+
+    public static int[] GenerateIndexArray(Array array)
+    {
+        int[] indices = new int[array.Length];
+        for (int i = 0; i < indices.Length; i++)
+            indices[i] = i;
+        return indices;
+    }
+
     #region 排序算法
     #region 归并排序
     /// <summary>
@@ -3802,6 +3975,56 @@ public static class ColorUtil
             }
         }
         return new string(array);
+    }
+
+    /// <summary>
+    /// 颜色过度
+    /// </summary>
+    /// <param name="colors"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static Color Lerp(Color[] colors, float t)
+    {
+        if (colors.Length == 1)
+            return colors[0];
+
+        int end = colors.Length - 1;
+
+        if (t >= 1.0f)
+        {
+            return colors[end];
+        }
+        else if (t <= 0.0f)
+        {
+            return colors[0];
+        }
+        else
+        {
+            float indOfs = t * end;
+            int ind = Mathf.FloorToInt(indOfs);
+
+            if (ind == end)
+                return colors[ind];
+
+            return Color.Lerp(colors[ind], colors[ind + 1], indOfs - ind);
+        }
+    }
+
+    public static Color LerpRepeat(Color[] colors, float t)
+    {
+        if (colors.Length == 1)
+            return colors[0];
+
+        int end = colors.Length - 1;
+
+        float indOfs = t * end;
+        int ind = Mathf.FloorToInt(indOfs) % end;
+
+        if (ind < 0) ind = end - ind;
+
+        int indNext = (ind + 1) % end;
+
+        return Color.Lerp(colors[ind], colors[indNext], indOfs - ind);
     }
 
     /// <summary>色码转颜色</summary>
