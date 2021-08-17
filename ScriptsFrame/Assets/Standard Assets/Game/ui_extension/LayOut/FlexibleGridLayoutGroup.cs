85d21ca4ac703930eb6b1fbc940a8246d883dd58
DESKTOP-8GLVILO\hua <2826593725@qq.com>
2021-08-17 17:05:51 +0800

    20210817 增加扩展的layout功能

---
+++ ScriptsFrame/Assets/Standard Assets/Game/ui_extension/LayOut/FlexibleGridLayoutGroup.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 增强版的layoutGroup，增加了指定行列和子对象自动铺满父对象的功能
+/// </summary>
+[AddComponentMenu("Layout/Flexible Grid Layout Group")]
+public class FlexibleGridLayoutGroup : LayoutGroup
+{
+    public enum FitType
+    {
+        Uniform,
+        Width,
+        Height,
+        FixedRows,
+        FixedColumns
+    }
+
+    public FitType fitType;
+
+    public int rows;
+    public int columns;
+    public Vector2 cellSize;
+    public Vector2 spacing;
+
+    public bool fitX = true;
+    public bool fitY = true;
+
+    public override void CalculateLayoutInputHorizontal()
+    {
+        base.CalculateLayoutInputHorizontal();
+
+        Logic();
+
+    }
+    public override void CalculateLayoutInputVertical()
+    {
+        Logic();
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+
+    }
+
+    public override void SetLayoutVertical()
+    {
+
+    }
+
+    private void Logic()
+    {
+        if (fitType == FitType.Width || fitType == FitType.Height || fitType == FitType.Uniform)
+        {
+            fitX = true;
+            fitY = true;
+
+            float sqrRt = Mathf.Sqrt(transform.childCount);
+            rows = Mathf.CeilToInt(sqrRt);
+            columns = Mathf.CeilToInt(sqrRt);
+        }
+
+        if (fitType == FitType.Width || fitType == FitType.FixedColumns)
+        {
+            rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+        }
+        if (fitType == FitType.Height || fitType == FitType.FixedRows)
+        {
+            columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+        }
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = (parentWidth / (float)columns) - ((spacing.x / (float)columns) * (columns - 1)) - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = (parentHeight / (float)rows) - ((spacing.y / (float)rows) * (rows - 1)) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+
+        cellSize.x = fitX ? cellWidth : cellSize.x;
+        cellSize.y = fitY ? cellHeight : cellSize.y;
+
+        int rowCount = 0;
+        int columnCount = 0;
+
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            rowCount = i / columns;
+            columnCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xPos = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
+            var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
+
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+    }
+}
